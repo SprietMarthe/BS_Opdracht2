09f56732df49f67728b68d867e3d76f3949c657d
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-04-25 18:00:15 +0200

    GUI beter

--- src/com/BS_Opdracht2_MariekeBeke_MartheSpriet/App.java
+++ src/com/BS_Opdracht2_MariekeBeke_MartheSpriet/App.java
@@ -15,22 +15,143 @@ import java.util.List;
 
 public class App {
     private JPanel panelMain;
+    private JButton executeOneProcessButton;
+    private JButton executeAllProcessesButton;
+    private JPanel PanelPageNumber;
+    private JPanel PanelPresentBit;
+    private JLabel PN15_PB;
+    private JLabel PN14_PB;
+    private JLabel PN13_PB;
+    private JLabel PN12_PB;
+    private JLabel PN11_PB;
+    private JLabel PN10_PB;
+    private JLabel PN9_PB;
+    private JLabel PN8_PB;
+    private JLabel PN7_PB;
+    private JLabel PN6_PB;
+    private JLabel PN5_PB;
+    private JLabel PN4_PB;
+    private JLabel PN3_PB;
+    private JLabel PN2_PB;
+    private JLabel PN1_PB;
+    private JLabel PN0_PB;
+    private JPanel PanelModifyBit;
+    private JLabel PN15_MB;
+    private JLabel PN14_MB;
+    private JLabel PN13_MB;
+    private JLabel PN12_MB;
+    private JLabel PN11_MB;
+    private JLabel PN10_MB;
+    private JLabel PN9_MB;
+    private JLabel PN8_MB;
+    private JLabel PN7_MB;
+    private JLabel PN6_MB;
+    private JLabel PN5_MB;
+    private JLabel PN4_MB;
+    private JLabel PN3_MB;
+    private JLabel PN2_MB;
+    private JLabel PN1_MB;
+    private JLabel PN0_MB;
+    private JPanel PanelLMT;
+    private JLabel PN0_LMT;
+    private JLabel PN1_LMT;
+    private JLabel PN2_LMT;
+    private JLabel PN3_LMT;
+    private JLabel PN4_LMT;
+    private JLabel PN5_LMT;
+    private JLabel PN6_LMT;
+    private JLabel PN7_LMT;
+    private JLabel PN8_LMT;
+    private JLabel PN9_LMT;
+    private JLabel PN10_LMT;
+    private JLabel PN11_LMT;
+    private JLabel PN12_LMT;
+    private JLabel PN13_LMT;
+    private JLabel PN14_LMT;
+    private JLabel PN15_LMT;
+    private JPanel PanelFrameNumber;
+    private JLabel PN0_FN;
+    private JLabel PN1_FN;
+    private JLabel PN2_FN;
+    private JLabel PN3_FN;
+    private JLabel PN4_FN;
+    private JLabel PN5_FN;
+    private JLabel PN6_FN;
+    private JLabel PN7_FN;
+    private JLabel PN_8FN;
+    private JLabel PN9_FN;
+    private JLabel PN10_FN;
+    private JLabel PN11_FN;
+    private JLabel PN12_FN;
+    private JLabel PN13_FN;
+    private JLabel PN14_FN;
+    private JLabel PN15_FN;
+    private JPanel PanelInstruction;
+    private JLabel InstructionLabel;
+    private JLabel InstrPIDLabel;
+    private JLabel InstrPIDValue;
+    private JLabel InstrOpLabel;
+    private JLabel InstrOpValue;
+    private JLabel InstrAddrLabel;
+    private JLabel InstrAddrValue;
+    private JLabel AddressLabel;
+    private JLabel AddrVirtLAbel;
+    private JLabel AddrVirtValue;
+    private JLabel AddrRealLabel;
+    private JLabel AddrRealValue;
+    private JLabel PageTableLabel;
+    private JPanel PanelInfoAndRam;
+    private JPanel PTPanel;
+    private JLabel RAMLabel;
+    private JLabel FrameNumberLabel;
+    private JLabel ProcessIDLabel;
+    private JLabel PageNumberLabel;
+    private JPanel FrameNumberPanel;
+    private JPanel ProcessIDPanel;
+    private JPanel PageNumberPanel;
+    private JLabel FN0;
+    private JLabel FN0_PID;
+    private JLabel FN1;
+    private JLabel FN1_PID;
+    private JLabel FN1_PN;
+    private JLabel FN2;
+    private JLabel FN2_PID;
+    private JLabel FN2_PN;
+    private JLabel FN3;
+    private JLabel FN3_PID;
+    private JLabel FN3_PN;
+    private JLabel FN4;
+    private JLabel FN4_PID;
+    private JLabel FN4_PN;
+    private JLabel FN5;
+    private JLabel FN5_PID;
+    private JLabel FN5_PN;
+    private JLabel FN6;
+    private JLabel FN6_PID;
+    private JLabel FN6_PN;
+    private JLabel FN7;
+    private JLabel FN7_PID;
+    private JLabel FN7_PN;
+    private JLabel FN8;
+    private JLabel FN8_PID;
+    private JLabel FN8_PN;
+    private JLabel FN9;
+    private JLabel FN9_PID;
+    private JLabel FN9_PN;
+    private JLabel FN10;
+    private JLabel FN10_PID;
+    private JLabel FN10_PN;
+    private JLabel FN11;
+    private JLabel FN11_PN;
+    private JLabel FN11_PID;
+    private JLabel FN0_PN;
+    private JPanel ButtonPanel;
+    private JPanel AddressPanel;
+    private JLabel TimerValue;
+    private JLabel TimerLabel;
+    private JPanel TimerPanel;
     private JButton allProcesses;
     private JButton oneProcess;
-    private JPanel ButtonPanel;
-    private JPanel InfoPanel;
-    private JTable PageTable;
-    private JTable RamTable;
-    private JLabel TimerLabel;
-    private JLabel TimerValue;
-    private JLabel InsturctieLabel;
-    private JLabel InstructieValue;
-    private JLabel AdresLabel;
-    private JLabel AdresValue;
-    private JLabel ScrijfopdrachtenLabel;
-    private JLabel SchrijfopdrachtenValue;
-    private JLabel RamLabel;
-    private JLabel PageTableLabel;
 
     static final int welkeXMLFile=1;
     static List<Process> present_process_list;
@@ -64,12 +185,23 @@ public class App {
         ram = new RAM(numberOfFrames);
     }
     private static void displayJFrame() {
-        JFrame frame= new JFrame("App");
+//        JFrame frame= new JFrame("App");
+//        frame.setContentPane(new App().panelMain);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
+//        frame.setSize(new Dimension(1200, 700));
+
+        JFrame frame = new JFrame("App2");
         frame.setContentPane(new App().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
         frame.setSize(new Dimension(1200, 700));
+        frame.getContentPane().setBackground(Color.white);
+        frame.getContentPane().getComponent(0).setBackground(Color.gray);
+        frame.getContentPane().getComponent(1).setBackground(Color.gray);
+
     }
     public static void readingWholeXMLFile(){
         File file = null;
@@ -111,6 +243,7 @@ public class App {
     private void executeOneInstruction() {
         Instruction instruction = instructions.poll();
         System.out.print(instruction);
+        System.out.println(present_process_list);
         assert instruction != null;
         if (instruction.getOperation() == OperationProcess.Start){
             operationStart(instruction);
@@ -156,7 +289,9 @@ public class App {
         System.out.print("Process toegevoegd: " + process);
         present_process_list.add(process);
         process_list.add(process);
-        addProcessToRAM();
+        addProcessToRAM(process);
+        System.out.print("c");
+
     }
 
 
@@ -164,16 +299,7 @@ public class App {
     private void removeProcessFromRAM() {
         int numberOfProcessesPresent = present_process_list.size();
         int numberOfFramesPerProcess = numberOfFrames/numberOfProcessesPresent;
-//        int[] frameNumbersOfProcess1 = new int[12];
-//        int[] frameNumbersOfProcess2 = new int[12];
-//        int[] frameNumbersOfProcess3 = new int[12];
-//        int[] frameNumbersOfProcess4 = new int[12];
-//
-//        for (int i=0; i<present_process_list.size(); i++){
-//            pidPresentProcesses[i] = present_process_list.get(i).getProcessID();
-//        }
         List<Frame> old_list_frames = ram.getList_frames();
-//        List<Frame> new_list_frames = new ArrayList<>();
 
         for (int i = 0; i<numberOfProcessesPresent; i++){
             for (int j=0; j<numberOfFramesPerProcess; j++){
@@ -188,13 +314,14 @@ public class App {
         System.out.println("numberOfFramesPerProcess: " + numberOfFramesPerProcess);
     }
 
-    private void addProcessToRAM() {
+    private void addProcessToRAM(Process process) {
         int numberOfProcessesPresent = present_process_list.size();
         int numberOfFramesPerProcess = numberOfFrames/numberOfProcessesPresent;
-        for (int i = 0; i<numberOfProcessesPresent; i++){
-            for (int j=0; j<numberOfFramesPerProcess; j++){
-                ram.getList_frames().get(i*numberOfFramesPerProcess + j)
-                        .setPid(present_process_list.get(i).getProcessID());
+
+        if(numberOfProcessesPresent == 1){
+            for(int i=0; i<numberOfFramesPerProcess; i++){
+                ram.getList_frames().get(i).setPid(present_process_list.get(0).getProcessID());
+                ram.getList_frames().get(i).setPagenummer(i);
             }
         }
         System.out.println("\nRam herverdeeld: " + ram);
@@ -206,21 +333,28 @@ public class App {
 
 
     public App() {
-        oneProcess.addActionListener(e -> {
+//        oneProcess.addActionListener(e -> {
+//            if (!instructions.isEmpty()){
+//                executeOneInstruction();
+//            }
+//            else
+//                System.out.println("All instructions are executed!");
+//
+////            //JOptionPane.showMessageDialog(null, "Hello world");
+////            TimerValue.setText(String.valueOf(timer));              // voorbeeld
+////            TimerValue.setVisible(true);
+//        });
+//        allProcesses.addActionListener(e -> {
+//            while(!instructions.isEmpty()){
+//                executeOneInstruction();
+//            }
+//        });
+        executeOneProcessButton.addActionListener(e -> {
             if (!instructions.isEmpty()){
                 executeOneInstruction();
             }
             else
                 System.out.println("All instructions are executed!");
-
-//            //JOptionPane.showMessageDialog(null, "Hello world");
-//            TimerValue.setText(String.valueOf(timer));              // voorbeeld
-//            TimerValue.setVisible(true);
-        });
-        allProcesses.addActionListener(e -> {
-            while(!instructions.isEmpty()){
-                executeOneInstruction();
-            }
         });
     }
 }
